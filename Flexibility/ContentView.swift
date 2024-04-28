9362c89d8baccdc81f276c072d735c1433bb65e4
Hayronsan <hayronsan@gmail.com>
2024-04-28 13:06:22 +0500

    Initial Commit

---
+++ Flexibility/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flexibility
+//
+//  Created by Nodir on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flexibility/FlexibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexibilityApp.swift
+//  Flexibility
+//
+//  Created by Nodir on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FlexibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
